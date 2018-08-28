f66ed7d5911abf402713bf60b2012c734dd0de16
christhopher.leao <christhopherleao@icloud.com>
2018-08-28 17:54:19 -0300

    creating resumable (chunk) upload input file

--- src/components/FileUpload/FileUpload.jsx
+++ src/components/FileUpload/FileUpload.jsx
@@ -1,29 +1,40 @@
 import React, { PureComponent } from 'react';
 import './FileUpload.scss';
 import io from 'socket.io-client';
+import Resumable from 'resumablejs';
 
 class FileUpload extends PureComponent {
   constructor(props) {
     super(props);
-    this.state = {
-      url: '',
-    };
-    const socket = io.connect();
-    socket.on('server', (data) => {
-      console.log(data);
-      socket.emit('client', { my: 'data' });
+    this.socket = io.connect();
+    this.r = new Resumable({
+      target: '/upload',
+      query: { path: 'uploads' },
+      simultaneousUploads: 2,
+      setChunkTypeFromFile: true,
+      prioritizeFirstAndLastChunk: true,
     });
   }
 
-  componentWillMount() {
+  componentDidMount() {
+    this.r.assignDrop(document.getElementById('upload'));
+    this.r.assignBrowse(document.getElementById('upload'));
+    this.r.on('fileAdded', (file, event) => {
+      console.log('added', event.target.files, file.fileName);
+      this.r.upload();
+    });
+
+    this.r.on('complete', () => {
+      console.log('FINISHED');
+    });
+    // this.socket.on('server', data => console.log('hi from server', data));
+    // this.socket.emit('client', { mounted: 'componentDidMount' });
   }
 
   render() {
-    const { url } = this.state;
-
     return (
       <section id="FileUpload">
-        <pre>{url}</pre>
+        <input type="file" name="upload" id="upload" />
       </section>
     );
   }
